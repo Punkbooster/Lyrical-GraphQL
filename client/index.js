346b5d337cf016b163f2033237cb498829488b2e
Punkbooster <ugliar.arsen@gmail.com>
2020-04-30 14:57:38 +0200

    Bind graphql query with the component

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client'
+// apollo provider is a glue layer between React app & graphQL server
 import { ApolloProvider } from 'react-apollo'
 
 import SongList from './components/SongList'
